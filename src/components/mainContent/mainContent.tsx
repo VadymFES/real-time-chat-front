395ff8c38f2db74553870a2222182daee4ab5567
Vadym Fesiuk <vadymfesiuk@gmail.com>
2023-11-20 00:09:56 +0100

    make base template for side pannel and chat menu

---
+++ src/components/mainContent/mainContent.tsx
@@ -0,0 +1,15 @@
+import styles from './mainContent.module.css';
+
+interface MainContentProps {
+    // props
+}
+
+const MainContent: React.FC<MainContentProps> = () => {
+  return (
+    <div className={styles.MainContent}>
+        <p>ijqfo</p>
+    </div>
+  );
+};
+
+export default MainContent;
