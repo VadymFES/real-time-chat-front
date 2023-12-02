eb8f21d634c6744ed226343e9d384a4023646123
Vadym Fesiuk <vadymfesiuk@gmail.com>
2023-12-02 16:38:17 +0100

    separated sent & recieved

--- src/components/mainContent/MessageComponent/message.tsx
+++ src/components/mainContent/MessageComponent/message.tsx
@@ -2,31 +2,46 @@
 
 import React from 'react';
 import styles from './message.module.css';
-import { Url } from 'next/dist/shared/lib/router/router';
 
 interface Message {
-    id: string;
-    text: string;
-    sender: string;
-    timestamp: string;
-    userAvatar: Url;
-  }
+  id: string;
+  text: string;
+  sender: string;
+  timestamp: string;
+  userAvatar: string;
+}
 
 interface MessageProps {
   message: Message;
 }
 
 const Message: React.FC<MessageProps> = ({ message }) => {
+  const isUserMessage = message.sender === 'User';
+
   return (
-    <div key={message.id} className={`${styles.message} ${message.sender === 'User' ? styles.userMessage : styles.otherMessage}`}>
-      <p className={styles.sender}>{message.sender}</p>
+    <div className={`${styles.message} ${isUserMessage ? styles.userMessage : styles.otherMessage}`}>
+      {!isUserMessage && (
+        <img
+          className={styles.userAvatar}
+          src={message.userAvatar}
+          alt="User Avatar"
+        />
+      )}
       <div className={styles.messageContent}>
-        <p className={styles.text}>{message.text}</p>
-        <span className={styles.timestamp}>{message.timestamp}</span>
-        {<img className={styles.userAvatar} src={message.userAvatar} alt="User Avatar" />}
+        <div className={styles.header}>
+          <span className={styles.sender}>{message.sender}</span>
+          <span className={styles.timestamp}>{message.timestamp}</span>
+        </div>
+        <div className={styles.text}>{message.text}</div>
       </div>
+      {isUserMessage && (
+        <img
+          className={styles.userAvatar}
+          src={message.userAvatar}
+          alt="User Avatar"
+        />
+      )}
     </div>
-    
   );
 };
 
