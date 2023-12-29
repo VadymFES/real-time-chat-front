b5d6e923ad627e46d6a53e25730b3067bad58b2b
Vadym Fesiuk <vadymfesiuk@gmail.com>
2023-12-30 00:01:29 +0100

    add colors to users text

--- src/components/mainContent/MessageComponent/message.tsx
+++ src/components/mainContent/MessageComponent/message.tsx
@@ -1,5 +1,3 @@
-// Message.tsx
-
 import React from 'react';
 import styles from './message.module.css';
 
@@ -8,39 +6,42 @@ interface Message {
   text: string;
   sender: string;
   timestamp: string;
-  userAvatar: string;
 }
 
 interface MessageProps {
   message: Message;
+  currentUser: string;
 }
 
-const Message: React.FC<MessageProps> = ({ message }) => {
-  const isUserMessage = message.sender === 'User';
+const userColors: { [key: string]: string } = {}; // Dictionary to store user colors
+
+const getRandomColor = (): string => {
+  // Generate a random color in hexadecimal format
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const getUserColor = (username: string): string => {
+  // Check if the user already has a color assigned
+  if (!userColors[username]) {
+    // If not, generate a random color and assign it
+    userColors[username] = getRandomColor();
+  }
+
+  return userColors[username];
+};
+
+const Message: React.FC<MessageProps> = ({ message, currentUser }) => {
+  const isUserMessage = message.sender === currentUser;
+  const textColor = isUserMessage ? '#000000' : getUserColor(message.sender);
 
   return (
     <div className={`${styles.message} ${isUserMessage ? styles.userMessage : styles.otherMessage}`}>
-      {!isUserMessage && (
-        <img
-          className={styles.userAvatar}
-          src={message.userAvatar}
-          alt="User Avatar"
-        />
-      )}
       <div className={styles.messageContent}>
-        <div className={styles.header}>
-          <span className={styles.sender}>{message.sender}</span>
-          <span className={styles.timestamp}>{message.timestamp}</span>
-        </div>
-        <div className={styles.text}>{message.text}</div>
+        <span className={styles.sender} style={{ color: textColor }}>
+          {`[${message.timestamp}] - <${message.sender}>`}
+        </span>
+        <span className={styles.text} style={{ color: textColor }}>{message.text}</span>
       </div>
-      {isUserMessage && (
-        <img
-          className={styles.userAvatar}
-          src={message.userAvatar}
-          alt="User Avatar"
-        />
-      )}
     </div>
   );
 };
