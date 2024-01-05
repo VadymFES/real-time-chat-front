d0a5d9469b14002f01b1918b74548edc5851618f
Vadym Fesiuk <vadymfesiuk@gmail.com>
2024-01-05 21:50:22 +0100

    add userId

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -2,12 +2,17 @@ import React, { Dispatch, SetStateAction } from 'react';
 
 interface UserContextProps {
   username: string;
+  userId: string;
+
   setUsername: Dispatch<SetStateAction<string>>;
+  setUserId: Dispatch<SetStateAction<string>>;
 }
 
 const UserContext = React.createContext<UserContextProps>({
   username: '',
-  setUsername: () => {},
+  setUsername: () => { },
+  setUserId: () => { },
+  userId: ''
 });
 
 export default UserContext;
