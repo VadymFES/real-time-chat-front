bd35e53ac3810de112cda2a6be6857073e2664c2
Vadym Fesiuk <vadymfesiuk@gmail.com>
2024-01-05 21:57:14 +0100

    small fixes

--- src/components/roomSelection/RoomSelection.tsx
+++ src/components/roomSelection/RoomSelection.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styles from './roomSelection.module.css';
 
 type RoomSelectionProps = {
-  onSelectRoom: (roomName: string) => void;
+  onSelectRoom: (roomName: string, roomId: string) => void;
   selectedRoom: string;
 };
 
@@ -18,7 +18,7 @@ const RoomSelection = ({ onSelectRoom, selectedRoom }: RoomSelectionProps) => {
   const [rooms, setRooms] = useState<Room[]>([]);
 
   useEffect(() => {
-    fetch('http://51.20.108.68/rooms/')
+    fetch('http://51.20.108.68/rooms/') 
       .then(response => response.json())
       .then(data => {
         setRooms(data); // Assuming data is an array of Room objects
@@ -43,13 +43,15 @@ const RoomSelection = ({ onSelectRoom, selectedRoom }: RoomSelectionProps) => {
       </section>
       <section className={styles.roomButtons}>
         {rooms.map(room => (
-          <button
+          <><button
             key={room.id}
-            onClick={() => onSelectRoom(room.name)}
+            onClick={() => onSelectRoom(room.name, room.id)}
             className={`${styles.tab} ${room.name === selectedRoom ? styles.activeTab : ''}`}
           >
             {room.name}
           </button>
+          <button 
+          className={styles.tab}>+</button></>
         ))}
       </section>
     </div>
