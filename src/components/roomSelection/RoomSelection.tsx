f506f84c1897a8bff4b247ef6a25dafe0ee438a6
Vadym Fesiuk <vadymfesiuk@gmail.com>
2024-01-07 00:42:37 +0100

    minor changes in tabs styles

--- src/components/roomSelection/RoomSelection.tsx
+++ src/components/roomSelection/RoomSelection.tsx
@@ -6,7 +6,6 @@ type RoomSelectionProps = {
   selectedRoom: string;
 };
 
-
 interface Room {
   id: string;
   name: string;
@@ -16,45 +15,51 @@ interface Room {
 
 const RoomSelection = ({ onSelectRoom, selectedRoom }: RoomSelectionProps) => {
   const [rooms, setRooms] = useState<Room[]>([]);
+  const [anyTabActive, setAnyTabActive] = useState(() => {
+    return localStorage.getItem('anyTabActive') === 'true';
+  });
 
   useEffect(() => {
     fetch('http://51.20.108.68/rooms/') 
       .then(response => response.json())
-      .then(data => {
-        setRooms(data); 
-      })
-      .catch(error => {
-        console.error('Error fetching rooms:', error);
-      });
+      .then(data => setRooms(data))
+      .catch(error => console.error('Error fetching rooms:', error));
   }, []);
 
+  useEffect(() => {
+    localStorage.setItem('anyTabActive', anyTabActive.toString());
+  }, [anyTabActive]);
+
+  const handleTabClick = (roomName: string, roomId: string) => {
+    onSelectRoom(roomName, roomId);
+    setAnyTabActive(true);
+  };
+
   return (
-    <div className={styles.tabContainer}>
+    <><div className={styles.tabContainer}>
       <section className={styles.topChatName}>
         <div className={styles.tabHeader}>
           <h1 className={styles.chatName}>Echo chat</h1>
           <div className={styles.supportSettings}>
-          <button className={styles.support}>Support</button>
-          <button className={styles.settings}>Settings</button>
+            <button className={styles.support}>Support</button>
+            <button className={styles.settings}>Settings</button>
           </div>
-         
-          <p> userName writting...</p>
+          <p>userName writing...</p>
         </div>
       </section>
-      <section className={styles.roomButtons}>
+    </div>
+    <section className={styles.roomButtons}>
         {rooms.map(room => (
-          <><button
+          <button
             key={room.id}
-            onClick={() => onSelectRoom(room.name, room.id)}
-            className={`${styles.tab} ${room.name === selectedRoom ? styles.activeTab : ''}`}
+            onClick={() => handleTabClick(room.name, room.id)}
+            className={`${styles.tab} ${anyTabActive ? styles.roundedTabs : ''} ${room.name === selectedRoom ? styles.activeTab : ''}`}
           >
             {room.name}
           </button>
-          <button 
-          className={styles.tab}>+</button></>
         ))}
-      </section>
-    </div>
+        <button className={`${styles.tab} ${anyTabActive ? styles.roundedTabs : ''}`}>+</button>
+      </section></>
   );
 };
 
